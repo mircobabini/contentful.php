1163dbaee554754878d88984eb335588505de65c
Mirco Babini <mirkolofio@gmail.com>
2013-10-25 15:13:28 +0200

    demo.php++

--- examples/demo.php
+++ examples/demo.php
@@ -10,15 +10,14 @@ $ACCESS_TOKEN = 'b4c0n73n7fu1';
 $client = Client::get ($ACCESS_TOKEN);
 
 // use it for your needs
-$client->space ('cfexampleapi');
+$space = $client->space ('cfexampleapi');
 
-$client->contentTypes ('cfexampleapi');
-$client->contentType ('cat', 'cfexampleapi');
+$contentTypes = $client->contentTypes ('cfexampleapi');
+$contentType = $client->contentType ('cat', 'cfexampleapi');
 
-$client->entries ('cfexampleapi');
-$client->entry ('nyancat', 'cfexampleapi');
+$entries = $client->entries ('cfexampleapi');
+$entry = $client->entry ('nyancat', 'cfexampleapi');
 
-$client->assets ('cfexampleapi');
-$client->asset ('nyancat', 'cfexampleapi');
-
+$assets = $client->assets ('cfexampleapi');
+$asset = $client->asset ('nyancat', 'cfexampleapi');
 
