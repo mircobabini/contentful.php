d48be4a7e064a8087a0cad9340f8c380fcb7d509
Mirco Babini <mirkolofio@gmail.com>
2013-10-25 13:03:02 +0200

    0.0.21

--- contentful.php
+++ contentful.php
@@ -1,12 +1,12 @@
 <?php
 /*!
- * Contentful PHP Client
+ * PHP client for Contentful's Content Delivery API
  *
  * Copyright 2013, Mirco Babini <mirkolofio@gmail.com>
  * Licensed under the GPL Version 2 license.
  * http://www.opensource.org/licenses/GPL-2.0
  * 
- * 0.0.10
+ * 0.0.21
  */
 define ('ROOT_PATH', dirname (__FILE__));
 require_once 'libraries/unirest-php/Unirest.php';
