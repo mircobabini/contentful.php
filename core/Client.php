f1cb385fe3c2c7692a77b37adf8e4cb40ab03568
Mirco Babini <mirkolofio@gmail.com>
2013-10-25 13:56:40 +0200

    Client++ thanks to Unirest++

--- core/Client.php
+++ core/Client.php
@@ -5,7 +5,11 @@ class Client {
 	 * @var array
 	 */
 	private static $clients = array ();
-
+	/**
+	 * @var Client
+	 */
+	private static $active_client;
+	
 	/**
 	 * @param string $access_token
 	 * @return Client
@@ -15,7 +19,14 @@ class Client {
 			self::$clients[$access_token] = new self ($access_token);
 		}
 		
-		return self::$clients[$access_token];
+		self::$active_client = self::$clients[$access_token];
+		return self::$active_client;
+	}
+	/**
+	 * @return Client
+	 */
+	public static function active_client () {
+		return self::$active_client;
 	}
 	
 	
@@ -27,61 +38,43 @@ class Client {
 	private function __construct ($access_token) {
 		$this->access_token = $access_token;
 	}
+	public function access_token () {
+		return "Bearer {$this->access_token}";
+	}
 	
 	
 	public function space ($space_name) {
-		$r = Unirest::get ("https://cdn.contentful.com/spaces/{$space_name}", array (
-			'Content-Type' => 'application/vnd.contentful.management.v1+json',
-			'Authorization' => "Bearer {$this->access_token}"
-		));
+		$r = Unirest::get ("https://cdn.contentful.com/spaces/{$space_name}");
 		
 		return $r->body;
 	}
 	public function contentTypes ($space_name) {
-		$r = Unirest::get ("https://cdn.contentful.com/spaces/{$space_name}/content_types", array (
-			'Content-Type' => 'application/vnd.contentful.management.v1+json',
-			'Authorization' => "Bearer {$this->access_token}"
-		));
+		$r = Unirest::get ("https://cdn.contentful.com/spaces/{$space_name}/content_types");
 		
 		return $r->body;
 	}
 	public function contentType ($id, $space_name) {
-		$r = Unirest::get ("https://cdn.contentful.com/spaces/{$space_name}/content_types/{$id}", array (
-			'Content-Type' => 'application/vnd.contentful.management.v1+json',
-			'Authorization' => "Bearer {$this->access_token}"
-		));
+		$r = Unirest::get ("https://cdn.contentful.com/spaces/{$space_name}/content_types/{$id}");
 		
 		return $r->body;
 	}
 	public function entries ($space_name) {
-		$r = Unirest::get ("https://cdn.contentful.com/spaces/{$space_name}/entries", array (
-			'Content-Type' => 'application/vnd.contentful.management.v1+json',
-			'Authorization' => "Bearer {$this->access_token}"
-		));
+		$r = Unirest::get ("https://cdn.contentful.com/spaces/{$space_name}/entries");
 		
 		return $r->body;
 	}
 	public function entry ($id, $space_name) {
-		$r = Unirest::get ("https://cdn.contentful.com/spaces/{$space_name}/entries/{$id}", array (
-			'Content-Type' => 'application/vnd.contentful.management.v1+json',
-			'Authorization' => "Bearer {$this->access_token}"
-		));
+		$r = Unirest::get ("https://cdn.contentful.com/spaces/{$space_name}/entries/{$id}");
 		
 		return $r->body;
 	}
 	public function assets ($space_name) {
-		$r = Unirest::get ("https://cdn.contentful.com/spaces/{$space_name}/assets", array (
-			'Content-Type' => 'application/vnd.contentful.management.v1+json',
-			'Authorization' => "Bearer {$this->access_token}"
-		));
+		$r = Unirest::get ("https://cdn.contentful.com/spaces/{$space_name}/assets");
 		
 		return $r->body;
 	}
 	public function asset ($id, $space_name) {
-		$r = Unirest::get ("https://cdn.contentful.com/spaces/{$space_name}/assets/{$id}", array (
-			'Content-Type' => 'application/vnd.contentful.management.v1+json',
-			'Authorization' => "Bearer {$this->access_token}"
-		));
+		$r = Unirest::get ("https://cdn.contentful.com/spaces/{$space_name}/assets/{$id}");
 		
 		return $r->body;
 	}
